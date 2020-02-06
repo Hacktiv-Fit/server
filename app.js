d03e8fd2aef151dfddcd2123b220dee6993ff02d
gandasipayung <gandasipayung14@gmail.com>
2020-02-06 18:01:17 +0700

    Login Register Manual dan Google done

---
+++ app.js
@@ -0,0 +1,19 @@
+if(process.env.NODE_ENV === 'development'){
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const cors = require('cors')
+const routes = require('./routes')
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`I Love Your ${PORT}` )
+})
